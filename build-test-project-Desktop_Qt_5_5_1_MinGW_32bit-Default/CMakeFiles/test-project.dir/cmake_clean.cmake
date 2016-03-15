1c9d88760726355d7b7da7be03faaeec685f12fd
Nazar Tkachuk <Nazar Tkachuk>
2016-03-15 06:29:19 +0200

    Added project files.

---
+++ build-test-project-Desktop_Qt_5_5_1_MinGW_32bit-Default/CMakeFiles/test-project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test-project.dir/main.cpp.obj"
+  "test-project.pdb"
+  "test-project.exe"
+  "test-project.exe.manifest"
+  "libtest-project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test-project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
